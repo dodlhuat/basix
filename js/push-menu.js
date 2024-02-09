ea91580123f25f448034d9700fd10e88aa41b105
Andi <abauer@asqs.net>
2024-02-09 09:22:44 +0100

    push menu click away

--- js/push-menu.js
+++ js/push-menu.js
@@ -2,22 +2,37 @@ const menu = {
     init() {
         document.querySelector('#navigation').addEventListener('change', function (event) {
             const content = document.querySelector('.push-content');
-            const menu = document.querySelector('.push-menu');
             const pushed = content.classList.contains('pushed');
-            const control_icon = document.querySelector('.navigation-controls .icon');
-            if (pushed) {
-                content.classList.remove('pushed');
-                menu.classList.remove('pushed');
-                control_icon.classList.remove('arrow-left');
-                control_icon.classList.add('arrow-right');
+            if (!pushed) {
+                content.addEventListener('click', clickNav);
             } else {
-                content.classList.add('pushed');
-                menu.classList.add('pushed');
-                control_icon.classList.remove('arrow-right');
-                control_icon.classList.add('arrow-left');
+                content.removeEventListener('click', clickNav);
             }
+            pushToggle();
         });
     }
+}
+
+const pushToggle = function () {
+    const content = document.querySelector('.push-content');
+    const menu = document.querySelector('.push-menu');
+    const pushed = content.classList.contains('pushed');
+    const control_icon = document.querySelector('.navigation-controls .icon');
+    if (pushed) {
+        content.classList.remove('pushed');
+        menu.classList.remove('pushed');
+        control_icon.classList.remove('arrow-left');
+        control_icon.classList.add('arrow-right');
+    } else {
+        content.classList.add('pushed');
+        menu.classList.add('pushed');
+        control_icon.classList.remove('arrow-right');
+        control_icon.classList.add('arrow-left');
+    }
+}
+
+const clickNav = function () {
+    document.querySelector('#navigation').click();
 }
 
 export {menu}
